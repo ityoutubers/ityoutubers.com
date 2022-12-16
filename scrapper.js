7c5fee53f05b0070d7a7c6e9b1f10d0915b6721f
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2022-12-16 22:20:48 +0100

    use prettier

--- scrapper.js
+++ scrapper.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path: "./.env.local"});
+require("dotenv").config({ path: "./.env.local" });
 
 const { youtube } = require("@googleapis/youtube");
 const { Client } = require("@notionhq/client");
@@ -12,11 +12,17 @@ const youtubeApi = youtube({
   auth: process.env.GOOGLE_API_KEY,
 });
 
-async function getTopicsQuery(start_cursor, database_id = process.env.NOTION_TOPICS_DB_UID) {
-  return notion.databases.query({ database_id, start_cursor })
+async function getTopicsQuery(
+  start_cursor,
+  database_id = process.env.NOTION_TOPICS_DB_UID
+) {
+  return notion.databases.query({ database_id, start_cursor });
 }
 
-async function getActiveMembersQuery(start_cursor, database_id = process.env.NOTION_DB_UID) {
+async function getActiveMembersQuery(
+  start_cursor,
+  database_id = process.env.NOTION_DB_UID
+) {
   return notion.databases.query({
     database_id,
     start_cursor,
@@ -26,7 +32,7 @@ async function getActiveMembersQuery(start_cursor, database_id = process.env.NOT
         equals: true,
       },
     },
-  })
+  });
 }
 
 async function getPaginatedData(queryFunc) {
@@ -35,7 +41,7 @@ async function getPaginatedData(queryFunc) {
   let page = await queryFunc();
   items = page.results;
 
-  while(page.has_more) {
+  while (page.has_more) {
     page = await queryFunc(page.next_cursor);
     items = items.concat(page.results);
   }
@@ -48,9 +54,9 @@ async function getActiveMembers() {
 }
 
 async function getTopics() {
-  const topics = (await getPaginatedData(getTopicsQuery)).map(topic => ({
+  const topics = (await getPaginatedData(getTopicsQuery)).map((topic) => ({
     id: topic.id,
-    name: topic.properties.Name.title[0]?.plain_text
+    name: topic.properties.Name.title[0]?.plain_text,
   }));
 
   return topics;
@@ -59,16 +65,18 @@ async function getTopics() {
 async function getYouTubeChannels() {
   let youtubeChannels = [];
 
-  const idsLens = item => item.properties['Channel ID'].rich_text[0]?.plain_text;
-  const topisLens = item => item.properties['Topic'].relation.map(({id}) => id);
+  const idsLens = (item) =>
+    item.properties["Channel ID"].rich_text[0]?.plain_text;
+  const topisLens = (item) =>
+    item.properties["Topic"].relation.map(({ id }) => id);
 
   let members = await getActiveMembers();
-  const ytChannelsIds = members.map(idsLens).filter(id => !!id);
+  const ytChannelsIds = members.map(idsLens).filter((id) => !!id);
 
-  const topics = members.reduce(function(acc, cur) {
+  const topics = members.reduce(function (acc, cur) {
     acc[idsLens(cur)] = topisLens(cur);
     return acc;
-  }, {})
+  }, {});
 
   while (ytChannelsIds.length > 0) {
     const res = await youtubeApi.channels.list({
@@ -79,32 +87,45 @@ async function getYouTubeChannels() {
     youtubeChannels = youtubeChannels.concat(res.data.items);
   }
 
-  const playlistIds = youtubeChannels.map(ytChannel => ytChannel.contentDetails.relatedPlaylists.uploads);
+  const playlistIds = youtubeChannels.map(
+    (ytChannel) => ytChannel.contentDetails.relatedPlaylists.uploads
+  );
 
   const videos = {};
   await Promise.all(
-    playlistIds.map(id => youtubeApi.playlistItems.list({
-          part: "id, snippet", 
-          playlistId: id, 
-          maxResults: 1
-        }).catch(res => ({}))
-    )).then(all => all.forEach(res => (
-      videos[res.data?.items[0]?.snippet?.videoOwnerChannelId] = {
-        publishedAt: res.data?.items[0]?.snippet?.publishedAt,
-        title: res.data?.items[0]?.snippet?.title,
-      })
-    ));
+    playlistIds.map((id) =>
+      youtubeApi.playlistItems
+        .list({
+          part: "id, snippet",
+          playlistId: id,
+          maxResults: 1,
+        })
+        .catch((res) => ({}))
+    )
+  ).then((all) =>
+    all.forEach(
+      (res) =>
+        (videos[res.data?.items[0]?.snippet?.videoOwnerChannelId] = {
+          publishedAt: res.data?.items[0]?.snippet?.publishedAt,
+          title: res.data?.items[0]?.snippet?.title,
+        })
+    )
+  );
 
   // TODO: delete extra fields
-  youtubeChannels.forEach(ytChannel => {
-    ytChannel['lastVideo'] = videos[ytChannel.id];
-    ytChannel['topics'] = topics[ytChannel.id];
+  youtubeChannels.forEach((ytChannel) => {
+    ytChannel["lastVideo"] = videos[ytChannel.id];
+    ytChannel["topics"] = topics[ytChannel.id];
   });
 
   return youtubeChannels;
 }
 
 Promise.all([
-  getTopics().then(data => fs.writeFile("./pages/api/topics.json", JSON.stringify(data))),
-  getYouTubeChannels().then(data => fs.writeFile("./pages/api/channels.json", JSON.stringify(data)))
-]).catch(console.error)
+  getTopics().then((data) =>
+    fs.writeFile("./pages/api/topics.json", JSON.stringify(data))
+  ),
+  getYouTubeChannels().then((data) =>
+    fs.writeFile("./pages/api/channels.json", JSON.stringify(data))
+  ),
+]).catch(console.error);
