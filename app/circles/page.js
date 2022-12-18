68175c22474dbc4acf7d304af3595dc7f1a59fa2
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2022-12-18 21:36:54 +0100

    add page with circles

---
+++ app/circles/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import _ from "lodash";
+
+import { orderBySubscribersDesc } from "../../lib/channels";
+
+import members from "../../data/members.json";
+import channels from "../../data/channels.json";
+
+export default function Page() {
+  const channelsSortedBySubs = [...channels, ...members].sort(
+    orderBySubscribersDesc
+  );
+
+  return (
+    <>
+      <p className="italic">Все каналы кружочками. И сообщество, и не очень.</p>
+      <div id="channels" className="overflow-hidden">
+        {channelsSortedBySubs.map(({ id, snippet, statistics }) => (
+          <div key={id} className="float-left not-prose">
+            <a href={`https://youtube.com/channel/${id}`}>
+              <Image
+                width={50}
+                height={50}
+                alt=""
+                className="rounded-full"
+                src={snippet.thumbnails.medium.url}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
