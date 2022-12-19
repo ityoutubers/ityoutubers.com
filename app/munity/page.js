a75a1e4680d98e1618e34d886ce4e4f7a2d6089d
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2022-12-19 19:04:13 +0100

    Add munity page

---
+++ app/munity/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import _ from "lodash";
+
+export default function Page() {
+  return (
+    <>
+      <p className="italic"></p>
+    </>
+  );
+}
