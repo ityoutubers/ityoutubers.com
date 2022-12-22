b0ea9d6b848d1721e09d417fcdc8217b18762195
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2022-12-22 19:43:43 +0100

    flag members videos

--- app/VideoCard.js
+++ app/VideoCard.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 
 export default function VideoCard({ channel }) {
-  const { id, snippet, statistics, isMember, lastVideo } = channel;
+  const { isMember, lastVideo } = channel;
 
   return (
-    <div className="text-sm leading-normal relative">
+    <div
+      className={`${
+        isMember ? "ityoutubers-member" : ""
+      } text-sm leading-normal relative`}
+    >
       <a
         className="not-prose block"
         href={`https://www.youtube.com/watch?v=${lastVideo.id}`}
@@ -16,7 +20,10 @@ export default function VideoCard({ channel }) {
           width={lastVideo.thumbnail.width}
         />
       </a>
-      <a className="" href={`https://www.youtube.com/watch?v=${lastVideo.id}`}>
+      <a
+        className="video-title"
+        href={`https://www.youtube.com/watch?v=${lastVideo.id}`}
+      >
         {lastVideo.title}
       </a>
     </div>
