b9c8bc4d3090b078c2c1aeb216a246b1c9c44fab
Dmitriy (Dima) Rozhkov <rojkov.dmitry@gmail.com>
2022-12-24 11:10:49 +0100

    Update head.js

--- app/head.js
+++ app/head.js
@@ -5,6 +5,16 @@ export default function Head({ params }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>ITYouTubers — сообщество создателей IT контента</title>
       <link rel="icon" type="image/png" href="/fav.png" />
+    
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QBYCDEYL37"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QBYCDEYL37');
+      </script>
     </>
   );
 }
