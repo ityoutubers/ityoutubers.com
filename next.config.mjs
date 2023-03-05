53079dc2ad5b27b165244672d36806aac30316c8
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2023-03-05 16:46:42 +0100

    add sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import { withSentryConfig } from "@sentry/nextjs";
+
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx"],
   reactStrictMode: true,
+  sentry: {
+    hideSourceMaps: true,
+  },
   experimental: {
     appDir: true,
   },
@@ -20,4 +26,16 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
