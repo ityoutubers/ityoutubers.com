34b558bfce620df1ba42e690ca71e521c8184d3a
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2023-06-10 22:50:32 +0200

    feat: send notifications on new videos

--- scrapper.js
+++ scrapper.js
@@ -4,25 +4,6 @@ import { getTopics, getYouTubeChannels } from "./lib/data.js";
 
 dotenv.config({ path: "./.env.local" });
 
-await getYouTubeChannels().then(() => console.log("done")).then(() => process.exit(0));
-
-// Promise.all([
-//   getTopics()
-//     .then((data) => data.sort((a, b) => (a.name > b.name ? 1 : -1)))
-//     .then((data) =>
-//       fs.writeFile("./data/topics.json", JSON.stringify(data, null, 2))
-//     ),
-//   getYouTubeChannels(getMembers(true))
-//     .then((data) =>
-//       data.map((i) => {
-//         i["isMember"] = true;
-//         return i;
-//       })
-//     )
-//     .then((data) =>
-//       fs.writeFile("./data/members.json", JSON.stringify(data, null, 2))
-//     ),
-//   getYouTubeChannels(getMembers(false)).then((data) =>
-//     fs.writeFile("./data/channels.json", JSON.stringify(data, null, 2))
-//   ),
-// ]).catch(console.error);
+await getYouTubeChannels()
+  .then(() => console.log("done"))
+  .then(() => setTimeout(() => process.exit(0), 1000 * 60)); // 1 minute timeout, don't want to deal with it now
