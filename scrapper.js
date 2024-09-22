2eb5daf2e01f5eb64f3f7de590df9887a4e0f7df
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-09-22 20:39:44 +0200

    fix: abort instead of exiting on hang

--- scrapper.js
+++ scrapper.js
@@ -5,4 +5,9 @@ dotenv.config({ path: "./.env.local" });
 
 await getYouTubeChannels()
   .then(() => console.log("done"))
-  .then(() => setTimeout(() => process.exit(0), 1000 * 60)); // 1 minute timeout, don't want to deal with it now
+  .then(() =>
+    setTimeout(() => {
+      console.log("Process didn't exit, aborting");
+      process.abort();
+    }, 1000 * 10)
+  ); // 1 minute timeout, don't want to deal with it now
