77add66aed4781fd2a7edba8e2197121ad9ac6e9
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-10-04 18:14:12 +0200

    fix: fixed hanging process?

--- scrapper.js
+++ scrapper.js
@@ -4,12 +4,4 @@ import { getYouTubeChannels } from "./lib/data.js";
 
 dotenv.config({ path: "./.env.local" });
 
-await getYouTubeChannels()
-  .then(() => console.log("done"))
-  .then(() =>
-    setTimeout(() => {
-      console.log("Process didn't exit, aborting");
-      whyIsNodeRunning();
-      process.abort();
-    }, 1000 * 60)
-  ); // 1 minute timeout, don't want to deal with it now
+await getYouTubeChannels().then(() => console.log("done"));
